513067f86e8f57c9427696ff75608026df265b70
Rabin Shrestha <sanayashe@gmail.com>
2023-11-04 22:32:33 +0545

    add links

---
+++ script.js
@@ -0,0 +1,27 @@
+// Sticky Navbar
+let header = document.querySelector("header");
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
+
+menu.onclick = () => {
+  navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
+
+// Hiding Navbar
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-72px";
+  }
+  prevScrollpos = currentScrollPos;
+};
